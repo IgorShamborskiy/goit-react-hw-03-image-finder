558f8d7a82f113ec612fc13e0b684e591584a70d
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2023-03-21 23:02:26 +0200

    add task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,17 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Modal } from './Modal/Modal';
-
+import { getImages } from '../Api/Api';
+import { toast } from 'react-toastify';
+const STATUS = {
+  idle: 'idle',
+  loading: 'loading',
+  succes: 'succes',
+  error: 'error',
+};
 export class App extends Component {
   state = {
+    imagesList: null,
     page: 1,
     search: '',
     status: '',
@@ -19,7 +27,35 @@ export class App extends Component {
     largeImage: null,
     totalhits: 0,
   };
-
+  async componentDidUpdate(prevState) {
+    // const nextRequest = this.props.searchImages;
+    // const prevRequest = prevProps.searchImages;
+    // const prevPage = this.props.page;
+    // const nextPage = prevProps.page;
+    const { onStatusChange } = this.props;
+    const { search, page } = this.state;
+    if (prevState.page !== page || prevState.search !== search) {
+      onStatusChange(STATUS.loading);
+      try {
+        const data = await getImages(page, prevState.search);
+        if (data.hits.length === 0) {
+          toast.warn(`Sorry! We didn't find anything, change your request`);
+          return;
+        }
+        onStatusChange(STATUS.succes);
+        this.setState({ imagesList: data });
+        this.props.onRecordingImagesList(data.hits);
+        this.props.onWriteTotalHits(data.totalHits);
+        if (prevState.search !== search) {
+          return;
+        }
+        toast.success(`Hooray! We found ${data.totalHits} images.`);
+      } catch (error) {
+        onStatusChange(STATUS.error);
+        toast.error('Opps! Something went wrong');
+      }
+    }
+  }
   loadMore = () => {
     this.setState(prevState => ({ page: prevState.page + 1 }));
   };
@@ -70,23 +106,8 @@ export class App extends Component {
     return (
       <AppStyled>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          page={page}
-          searchImages={search}
-          onStatusChange={this.changeStatus}
-          onRecordingImagesList={this.recordingImagesList}
-          onWriteTotalHits={this.writeTotalHits}
-        >
-          {imagesList?.map(image => (
-            <ImageGalleryItem
-              key={image.id}
-              image={image.webformatURL}
-              onToggle={this.toggleModal}
-              writeLargeImage={this.writeLargeImage}
-              bigImg={image.largeImageURL}
-            ></ImageGalleryItem>
-          ))}
-        </ImageGallery>
+        {!!imagesList?.length && <ImageGallery imagesList={imagesList} />}
+
         {status === 'loading' && <Loader />}
         <ToastContainer autoClose={2000} />
         {showModal && (
