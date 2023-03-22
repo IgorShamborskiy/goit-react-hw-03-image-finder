5625dd1aa99587da6e1ccadbb7adf289d87684c8
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2023-03-22 21:26:52 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,131 +1,16 @@
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Component } from 'react';
-import { AppStyled } from './App.styled';
-import { Loader } from './Loader/Loader';
-import { ButtonLoadMore } from './Button/Button';
-import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
-import { Modal } from './Modal/Modal';
-import { getImages } from '../Api/Api';
-import { toast } from 'react-toastify';
-const STATUS = {
-  idle: 'idle',
-  loading: 'loading',
-  succes: 'succes',
-  error: 'error',
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
-export class App extends Component {
-  state = {
-    imagesList: [],
-    page: 1,
-    search: '',
-    status: '',
-    imagesList: null,
-    showModal: false,
-    largeImage: null,
-    totalhits: 0,
-  };
-  async componentDidUpdate(prevState) {
-    // const nextRequest = this.props.searchImages;
-    // const prevRequest = prevProps.searchImages;
-    // const prevPage = this.props.page;
-    // const nextPage = prevProps.page;
-    const { onStatusChange } = this.props;
-    const { search, page } = this.state;
-    if (prevState.page !== page || prevState.search !== search) {
-      onStatusChange(STATUS.loading);
-      try {
-        const data = await getImages(page, prevState.search);
-        if (data.hits.length === 0) {
-          toast.warn(`Sorry! We didn't find anything, change your request`);
-          return;
-        }
-
-        this.setState(prevState => ({
-          imagesList: [prevState.images, ...data.hits],
-          totalhits: data.totalhits,
-        }));
-        // this.props.onRecordingImagesList(data.hits);
-        // this.props.onWriteTotalHits(data.totalHits);
-        if (page === 1) {
-          toast.success(`Hooray! We found ${data.totalHits} images.`);
-        }
-      } catch (error) {
-        onStatusChange(STATUS.error);
-        toast.error('Opps! Something went wrong');
-      } finally {
-        onStatusChange(STATUS.succes);
-      }
-    }
-  }
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  changeStatus = value => {
-    this.setState({ status: value });
-  };
-
-  handleFormSubmit = search => {
-    this.setState({ page: 1, search, imagesList: [], totalhits: 0 });
-  };
-
-  // recordingImagesList = data => {
-  //   if (!this.state.imagesList) {
-  //     this.setState({ imagesList: data });
-  //     return;
-  //   }
-  //   if (this.state.imagesList) {
-  //     this.setState(prevState => ({
-  //       imagesList: [...prevState.imagesList, ...data],
-  //     }));
-  //     return;
-  //   }
-  // };
-
-  writeLargeImage = largeImage => {
-    this.setState({ largeImage });
-  };
-
-  writeTotalHits = totalhits => {
-    this.setState({ totalhits });
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  render() {
-    const {
-      page,
-      search,
-      status,
-      imagesList,
-      showModal,
-      largeImage,
-      totalhits,
-    } = this.state;
-    return (
-      <AppStyled>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {!!imagesList?.length && (
-          <ImageGallery
-            imagesList={imagesList}
-            writeLargeImage={writeLargeImage}
-          ></ImageGallery>
-        )}
-
-        {status === 'loading' && <Loader />}
-        <ToastContainer autoClose={2000} />
-        {showModal && (
-          <Modal largeImg={largeImage} onToggle={this.toggleModal} />
-        )}
-        {status !== 'loading' && imagesList !== totalhits > 12 && (
-          <ButtonLoadMore loadMore={this.loadMore} />
-        )}
-      </AppStyled>
-    );
-  }
-}
--- src/components/Loader/Loader.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { ThreeDots } from 'react-loader-spinner';
-
-export const Load = styled(ThreeDots)`
-  justify-content: center;
-`;
